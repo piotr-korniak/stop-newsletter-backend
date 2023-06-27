c09171831b27a435fe5d0096ed7ae1073c6264d6
Piotr Korniak <piotr@korniak.pl>
2023-06-27 07:36:07 +0200

    Initial commit

---
+++ src/main/java/com/stopnewsletter/backend/StopNewsletterApplication.java
@@ -0,0 +1,13 @@
+package com.stopnewsletter.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopNewsletterApplication {
+
+	public static void main( String[] args) {
+		SpringApplication.run( StopNewsletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stopnewsletter/backend/StopNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stopnewsletter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopNewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
